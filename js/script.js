1088debc079033ff8bef4b8f3972830f2b075415
Matisse <artamation@gmail.com>
2021-03-25 19:51:12 +0000

    formatted a bit of css and messed around with locales

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 
 const clock = function() {
+  // const options = {
+  //   hour: 'numeric',
+  //   minute: 'numeric',
+  //   day: 'numeric',
+  //   month: 'numeric',
+  //   year: 'numeric',
+  //   // weekday: 'long',
+  // };
   let newdate = new Date();
 
   const currentTime = function() {
@@ -7,7 +15,9 @@ const clock = function() {
     const hours = formatDate(newdate.getHours());
     const minutes = formatDate(newdate.getMinutes());
     const seconds = formatDate(newdate.getSeconds());
-
+    const month = formatDate(newdate.getMonth());
+    const day = formatDate(newdate.getDay());
+    document.querySelector('.monthDay').innerText = `${month}:${day}`;
     document.querySelector('.clockdata').innerText = `${hours}:${minutes}:${seconds}`;
     let t = setTimeout(function(){currentTime()} , 1000);
   };
@@ -16,3 +26,17 @@ const clock = function() {
   currentTime();
 }
 clock();
+
+
+
+
+//
+// const now = new Date();
+// const options = {
+//   hour: 'numeric',
+//   minute: 'numeric',
+//   day: 'numeric',
+//   month: 'numeric',
+//   year: 'numeric',
+//   // weekday: 'long',
+// };
