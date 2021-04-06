969f17886e1532e3a9a5939931276c76268de9ff
Matisse <artamation@gmail.com>
2021-04-06 12:36:54 -0400

    Made the basic functionality of a options menue

--- js/script.js
+++ js/script.js
@@ -18,26 +18,26 @@ const clock = function() {
     document.querySelector('.monthDay').innerText = new Intl.DateTimeFormat(locale, monthDay).format(newdate);
     document.querySelector('.clockdata').innerText = new Intl.DateTimeFormat(locale, hoursMinutes).format(newdate);
     let t = setTimeout(function(){currentTime()} , 1000);
+    //setting the timeout to 1000 will make it work well as a timer but will add a delay to actions we can lower the intercal timer to somthing lower but i dont want to sacrifice preformence right now
   };
   currentTime();
 
-// proof that i can change options of the clock with command
-  document.querySelector('.clickMe').addEventListener('click', (e) => monthDay.month =  'long');
 
 
+  const optionMenuTrigger = function() {
+    // document.querySelector('section#options').style.display =  'block' ? 'none' : 'block';
+     // console.log(document.getElementById('#options').classList.length);
+     const options = document.querySelector('#options');
+     options.classList.contains('hidden') ? options.classList.remove('hidden') : options.classList.add('hidden');
+  }
+
+  document.querySelector('.clickMe').addEventListener('click', optionMenuTrigger);
+
+
+// cycling month format options
+  document.querySelector('#month-narrow').addEventListener('click', (e) => monthDay.month =  'narrow');
+  document.querySelector('#month-short').addEventListener('click', (e) => monthDay.month =  'short');
+  document.querySelector('#month-long').addEventListener('click', (e) => monthDay.month =  'long');
+  document.querySelector('#month-numeric').addEventListener('click', (e) => monthDay.month =  'numeric');
 }
 clock();
-
-
-
-
-//
-// const now = new Date();
-// const options = {
-//   hour: 'numeric',
-//   minute: 'numeric',
-//   day: 'numeric',
-//   month: 'numeric',
-//   year: 'numeric',
-//   // weekday: 'long',
-// };
