36b7ff5e4fd57bfd59752bf8cd7767a344e15c39
Matisse <artamation@gmail.com>
2021-04-09 13:58:32 -0400

    finished formatting options for day and month, added a text field to change the main colors of the page with

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
+'use strict';
+const monthDay = {
+  month: 'numeric',
+  day: 'numeric',
 
+}
 const clock = function() {
   const hoursMinutes = {
     hour: 'numeric',
     minute: 'numeric',
     second: 'numeric'
   };
-  const monthDay = {
-    month: 'numeric',
-    day: 'numeric',
 
-  }
   let newdate = new Date();
 
   const locale = navigator.language;
@@ -18,6 +19,8 @@ const clock = function() {
     document.querySelector('.monthDay').innerText = new Intl.DateTimeFormat(locale, monthDay).format(newdate);
     document.querySelector('.clockdata').innerText = new Intl.DateTimeFormat(locale, hoursMinutes).format(newdate);
     let t = setTimeout(function(){currentTime()} , 1000);
+    document.querySelector('#switchMonth').innerText = monthDay.month;
+    document.querySelector('#switchDay').innerText = monthDay.day;
     //setting the timeout to 1000 will make it work well as a timer but will add a delay to actions we can lower the intercal timer to somthing lower but i dont want to sacrifice preformence right now
   };
   currentTime();
@@ -34,10 +37,62 @@ const clock = function() {
   document.querySelector('.clickMe').addEventListener('click', optionMenuTrigger);
 
 
-// cycling month format options
-  document.querySelector('#month-narrow').addEventListener('click', (e) => monthDay.month =  'narrow');
-  document.querySelector('#month-short').addEventListener('click', (e) => monthDay.month =  'short');
-  document.querySelector('#month-long').addEventListener('click', (e) => monthDay.month =  'long');
-  document.querySelector('#month-numeric').addEventListener('click', (e) => monthDay.month =  'numeric');
+// cycling month format options the options should be narrow, short, long, and numeric I could try doing this with select element
+  const switchMonth = function() {
+    switch (monthDay.month) {
+      case 'numeric':
+        monthDay.month =  'narrow';
+        break;
+      case 'narrow':
+        monthDay.month =  'short';
+        break;
+      case 'short':
+        monthDay.month =  'long';
+        break;
+      case 'long':
+        monthDay.month =  '2-digit';
+        break;
+      case '2-digit':
+        monthDay.month =  'numeric';
+        break;
+      default:
+        alert('error');
+        break;
+    }
+  }
+  const switchDay = function() {
+    switch (monthDay.day) {
+      case 'numeric':
+        monthDay.day = '2-digit'
+        break;
+        case '2-digit':
+          monthDay.day =  'numeric';
+          break;
+      default:
+
+    }
+  }
+  document.querySelector('#switchMonth').addEventListener('click', switchMonth);
+  document.querySelector('#switchDay').addEventListener('click', switchDay);
+
+
+
+
+  function myFunction_set() {
+    const mainColor =  document.querySelector("#mainColor").value;
+    if (/^#([0-9A-F]{3}){1,2}$/i.test(mainColor)) {
+      document.querySelector(':root').style.setProperty('--main-color', mainColor);
+    }else{
+      alert('enter a hex color');
+    }
 }
+document.querySelector('#mainColorSubmit').addEventListener('click', myFunction_set);
+
+
+}
+ //  how to check hex                 /^#[0-9A-F]{6}$/i.test('#fffff')
+
+
+
+
 clock();
