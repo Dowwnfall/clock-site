578afc6b46812ccf62002d71c5960db5546fd609
Matisse <artamation@gmail.com>
2021-04-11 22:58:26 -0400

    Added start of help menu.  Added different background images to choose from, added a way to change main-color vaiable in the stylesheet.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const monthDay = {
 
 }
 const clock = function() {
+  let backgroundNumber = 0;
+
   const hoursMinutes = {
     hour: 'numeric',
     minute: 'numeric',
@@ -19,23 +21,28 @@ const clock = function() {
     document.querySelector('.monthDay').innerText = new Intl.DateTimeFormat(locale, monthDay).format(newdate);
     document.querySelector('.clockdata').innerText = new Intl.DateTimeFormat(locale, hoursMinutes).format(newdate);
     let t = setTimeout(function(){currentTime()} , 1000);
-    document.querySelector('#switchMonth').innerText = monthDay.month;
-    document.querySelector('#switchDay').innerText = monthDay.day;
+    document.querySelector('#switchMonth').innerText = `Month:${monthDay.month}`;
+    document.querySelector('#switchDay').innerText = `Day:${monthDay.day}`;
     //setting the timeout to 1000 will make it work well as a timer but will add a delay to actions we can lower the intercal timer to somthing lower but i dont want to sacrifice preformence right now
   };
   currentTime();
 
 
 
-  const optionMenuTrigger = function() {
-    // document.querySelector('section#options').style.display =  'block' ? 'none' : 'block';
-     // console.log(document.getElementById('#options').classList.length);
-     const options = document.querySelector('#options');
-     options.classList.contains('hidden') ? options.classList.remove('hidden') : options.classList.add('hidden');
+  const menuTrigger = function() {
+    const idthing = this;
+     const idSelector = document.querySelector(idthing);
+     if (idSelector.classList.contains('hidden')) {
+       idSelector.classList.remove('hidden')
+       idSelector.classList.add('menu-on')
+     } else {
+       idSelector.classList.remove('menu-on')
+       idSelector.classList.add('hidden')
+     }
   }
 
-  document.querySelector('.clickMe').addEventListener('click', optionMenuTrigger);
-
+  document.querySelector('.optionsButton').addEventListener('click', menuTrigger.bind('#options'));
+  document.querySelector('.helpButton').addEventListener('click', menuTrigger.bind('#help'));
 
 // cycling month format options the options should be narrow, short, long, and numeric I could try doing this with select element
   const switchMonth = function() {
@@ -72,9 +79,18 @@ const clock = function() {
 
     }
   }
+
+  const switchBackground = function() {
+    document.querySelector('body').classList.remove(`background${backgroundNumber}`);
+    backgroundNumber < 3 ? backgroundNumber++ : backgroundNumber = 0;
+    document.querySelector('body').classList.add(`background${backgroundNumber}`);
+  }
+
+
+
   document.querySelector('#switchMonth').addEventListener('click', switchMonth);
   document.querySelector('#switchDay').addEventListener('click', switchDay);
-
+  document.querySelector('#switchBackground').addEventListener('click', switchBackground);
 
 
 
